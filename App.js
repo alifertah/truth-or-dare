768658cca7bf59ee3c0817419011a02b667985ef
alifertah <alfertah@student.1337.ma>
2023-05-07 15:20:04 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { s } from 'react-native-wind'
+import Head from './components/head';
+import { useState } from 'react';
+
+const actions = [{name:"Truth", fun:"fetchTruth", value:"truth"},
+{name:"dare", fun:"fetchDare", value:"dare"}
+]
 
 export default function App() {
+  const [truth, setTruth] = useState("")
+  const [tod, setTod] = useState("")
+
+  const fetchTruth = async() =>{
+    try{
+      const response = await fetch("https://api.truthordarebot.xyz/v1/truth")
+      const data = await response.json()
+      if(tod === "truth")
+        setTruth(data.question)
+      else
+        setTruth("he")
+      }
+    catch(error){
+      console.log("ERROR")
+    }
+  }
+
+  const setAndCall= (actionName)=>{
+    setTod(actionName);
+    fetchTruth()
+  }
+
+  const renderActions = () => {
+    return actions.map((action, index) => (
+      <View style={s` bg-red-100 justify-center items-center w-full`}>
+        <TouchableOpacity key={index} onPress={()=>setAndCall(action.value)}>
+          <Text>{action.name}</Text>
+        </TouchableOpacity>
+      </View>
+    ));
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={s`flex justify-center items-center h-full`}>    
+    {renderActions()}
+    <Text>{truth}</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/head.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Head(){
+    return(
+        <View>
+
+            <Text>
+                Hilo
+            </Text> 
+        </View>
+    )
+}
