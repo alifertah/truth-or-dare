03df9cdffd271edbd09cc8ec8dd712a97a3947b4
alifertah <alfertah@student.1337.ma>
2023-05-07 15:33:00 +0100

    everything work as expected no til now

--- App.js
+++ App.js
@@ -11,30 +11,41 @@ const actions = [{name:"Truth", fun:"fetchTruth", value:"truth"},
 export default function App() {
   const [truth, setTruth] = useState("")
   const [tod, setTod] = useState("")
+  
+  const setAndCall= (actionName)=>{
+    if(actionName === "truth")
+      fetchTruth()
+    else
+      fetchDare()
+  }
 
   const fetchTruth = async() =>{
     try{
       const response = await fetch("https://api.truthordarebot.xyz/v1/truth")
       const data = await response.json()
-      if(tod === "truth")
-        setTruth(data.question)
-      else
-        setTruth("he")
+      setTruth(data.question)  
+    }
+    catch(error){
+      console.log("ERROR")
+    }
+  }
+
+
+  const fetchDare = async() =>{
+    try{
+      const response = await fetch("https://api.truthordarebot.xyz/api/dare")
+      const data = await response.json()
+      setTruth(data.question)  
       }
     catch(error){
       console.log("ERROR")
     }
   }
 
-  const setAndCall= (actionName)=>{
-    setTod(actionName);
-    fetchTruth()
-  }
-
   const renderActions = () => {
     return actions.map((action, index) => (
       <View style={s` bg-red-100 justify-center items-center w-full`}>
-        <TouchableOpacity key={index} onPress={()=>setAndCall(action.value)}>
+        <TouchableOpacity style={s`py-5`} key={index} onPress={()=>setAndCall(action.value)}>
           <Text>{action.name}</Text>
         </TouchableOpacity>
       </View>
